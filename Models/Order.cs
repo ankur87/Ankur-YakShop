e448308d20c3be96a1657883a815981e94d71a8b
ankur87 <30698326+ankur87@users.noreply.github.com>
2017-08-04 10:06:09 +0530

    Create Order.cs

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+public class Orders
+{
+    public string customer { get; set; }
+    public Order order { get; set; }
+}
+
+public class Order
+{
+    public int milk { get; set; }
+    public int skins { get; set; }
+}
